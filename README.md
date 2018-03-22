49a1d9b369423e04e94f804df456e9006c57a4c6
I-Chiao Hsu <ichiao.hsu@gmail.com>
2018-03-23 00:27:38 +0800

    Increase request efficiency 1. Incorporated goroutine and channel to realize asynchronous request 2. Filter duplicate player

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # player-lister
+
+A simple asynchronous http request script to request API for players in target teams. After eliminating players showed up in both club and national team, it will print results to stdout.
+
+## Environment
+
+Please use go >= 1.8. Recommended 1.9.2
+
+## Usage
+
+Clone this repository or `main.go` under `$GOPATH/src/`. In the same directory with `main.go` use command
+```bash
+go run main.go
+```
+
+to execute.
--- main.go
+++ main.go
@@ -12,14 +12,18 @@ import (
 
 const url = "https://vintagemonster.onefootball.com/api/teams/en/%v.json"
 
+// Player is the struct to store necessary player information
+// for Unmarshaled JSON
 type Player struct {
 	Country string `json:"country"`
 	Name    string `json:"name"`
 	Age     string `json:"age"`
 }
 
+// Players is used for storing results of total players
 type Players []Player
 
+// SingleTeam store result of each team in every API request
 type SingleTeam struct {
 	Status  string `json:"status"`
 	Code    int    `json:"code"`
@@ -32,7 +36,8 @@ type SingleTeam struct {
 	} `json:"data"`
 }
 
-func isTeamDesired(name string, target []string) bool {
+func isTeamDesired(name string) bool {
+	var target = []string{"Germany", "England", "France", "Spain", "Manchester Utd", "Arsenal", "Chelsea", "Barcelona", "Real Madrid", "FC Bayern Munich"}
 	for _, v := range target {
 		if name == v {
 			return true
@@ -41,51 +46,81 @@ func isTeamDesired(name string, target []string) bool {
 	return false
 }
 
-func main() {
-
-	var desiredTeams = []string{"Germany", "England", "France", "Spain", "Manchester Utd", "Arsenal", "Chelsea", "Barcelona", "Real Madrid", "FC Bayern Munich"}
-	var httpClient = &http.Client{
-		Timeout: time.Second * 10,
-	}
-	var result Players
-	teamID := 1
-	teamCount := 0
-
-	for {
-		response, err := httpClient.Get(fmt.Sprintf(url, teamID))
-		if err != nil {
-			panic(err)
+func asyncHTTPGet(id int, jobs <-chan int, results chan<- Players) {
+	for j := range jobs {
+		var httpClient = &http.Client{
+			Timeout: time.Second * 10,
 		}
-		defer response.Body.Close()
-
-		body, err := ioutil.ReadAll(response.Body)
+		resp, err := httpClient.Get(fmt.Sprintf(url, j))
 		if err != nil {
-			panic(err)
+			fmt.Printf("Team %d GET request error: %v\n", j, err.Error())
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Team %d read response body err: %v\n", j, err.Error())
 		}
 		var jsonResult = SingleTeam{}
 		if err = json.Unmarshal(body, &jsonResult); err != nil {
-			panic(err)
+			fmt.Printf("Team %d Unmarshl JSON response body err: %v\n", j, err.Error())
 		}
 		if jsonResult.Code != 0 && jsonResult.Code == 404 {
 			if strings.Contains(jsonResult.Message, "not find team with id") {
-				break
+				fmt.Printf("Team %d Not Found\n", j)
+				results <- Players{}
 			}
-			panic(err)
 		}
-		if isTeamDesired(jsonResult.Data.Team.Name, desiredTeams) {
-			result = append(result, jsonResult.Data.Team.Players...)
-			teamCount++
+		if isTeamDesired(jsonResult.Data.Team.Name) {
+			results <- jsonResult.Data.Team.Players
 		}
-		if teamCount == len(desiredTeams) {
+	}
+}
+
+func main() {
+
+	jobs := make(chan int, 3)
+	ch := make(chan Players, 10)
+
+	var results Players
+	var nonDuplicate = make(map[string]Player)
+
+	for w := 1; w <= 10; w++ {
+		go asyncHTTPGet(w, jobs, ch)
+	}
+
+	teamID := 1
+	for {
+		jobs <- teamID
+
+		// Break for loop if result channel is full
+		if len(ch) == cap(ch) {
 			break
 		}
 		teamID++
 	}
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].Name < result[j].Name
+	close(jobs)
+
+	for i := 1; i <= 10; i++ {
+		res := <-ch
+		for _, player := range res {
+			// If the name of each player is not key in nonDuplicate
+			// insert this player into nonDuplicate
+			if _, ok := nonDuplicate[player.Name]; !ok {
+				nonDuplicate[player.Name] = player
+			}
+		}
+	}
+
+	// Append non-duplicate players to final result slice
+	for _, player := range nonDuplicate {
+		results = append(results, player)
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Name < results[j].Name
 	})
 
-	for _, value := range result {
+	for _, value := range results {
 		fmt.Printf("%s; %s; %s\n", value.Name, value.Age, value.Country)
 	}
 }
